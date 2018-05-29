2d4d48ac2b8f95b6b3c4b15a5f4a6e2f4ebb9e63
luigimj <luigimendozajibaja@gmail.com>
2018-05-28 22:03:21 -0500

    Attribute Binding

--- vue-instance/main.js
+++ vue-instance/main.js
@@ -3,6 +3,8 @@ var product = "Socks";
 var app = new Vue({
   el: '#app',
   data: {
-    product: 'Socks'
+    product: 'Socks',
+    description: 'A pair of warm, fuzzy socks',
+    image: 'https://d2mxuefqeaa7sj.cloudfront.net/s_ACF2B3FED5F7644A8E27E3FE8A9142BB95ECC3C792EA9166BF492FA2116B5277_1517608730821_Screen+Shot+2018-02-02+at+4.58.29+PM.png'
   }
 });
